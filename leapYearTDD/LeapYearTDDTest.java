699560ef4b1923f1f69c508406796c7498d2bcc3
Rahul Joshi <rjoshi@ibaset.com>
2020-04-28 14:47:54 +0530

    General test cases

--- leapYearTDD/LeapYearTDDTest.java
+++ leapYearTDD/LeapYearTDDTest.java
@@ -22,4 +22,17 @@ class LeapYearTDDTest {
 	void testDivisibleBy4And100And400() {
 		assertTrue(leapYearTDD.isLeapYear(2000));
 	}
+
+	@Test
+	void testAllCases() {
+		assertFalse(leapYearTDD.isLeapYear(1700));
+		assertFalse(leapYearTDD.isLeapYear(1900));
+		assertFalse(leapYearTDD.isLeapYear(2100));
+		assertTrue(leapYearTDD.isLeapYear(2008));
+		assertTrue(leapYearTDD.isLeapYear(2012));
+		assertTrue(leapYearTDD.isLeapYear(2016));
+		assertFalse(leapYearTDD.isLeapYear(2017));
+		assertFalse(leapYearTDD.isLeapYear(2018));
+		assertFalse(leapYearTDD.isLeapYear(2019));
+	}
 }
