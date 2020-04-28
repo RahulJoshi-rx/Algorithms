0a6ee95b2d9a8856fd709a40bd66020fc27bde06
Rahul Joshi <rjoshi@ibaset.com>
2020-04-28 13:57:01 +0530

    Initial commit

---
+++ leapYearTDD/LeapYearTDD.java
@@ -0,0 +1,10 @@
+package leapYearTDD;
+
+public class LeapYearTDD {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hello");
+	}
+
+}
---
+++ leapYearTDD/LeapYearTDDTest.java
@@ -0,0 +1,14 @@
+package leapYearTDD;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class LeapYearTDDTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
